3877683a2da358f3dc4cf870452119feddfe6035
naikpriti <91533691+naikpriti@users.noreply.github.com>
2021-11-11 17:55:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "cluster" {
   source                = "./modules/cluster/"
   serviceprinciple_id   = var.serviceprinciple_id
   serviceprinciple_key  = var.serviceprinciple_key
-  ssh_key               = var.ssh_key
+#  ssh_key               = var.ssh_key
   location              = var.location
   kubernetes_version    = var.kubernetes_version  
   
