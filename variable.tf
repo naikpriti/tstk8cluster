51e8bafecb9dfc3afcf28329b6f55bc53b4a1c58
naikpriti <91533691+naikpriti@users.noreply.github.com>
2021-11-11 17:24:44 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "serviceprinciple_id" {
+    default = "688e984b-0494-4687-8d6b-5e59e2cc5042"
+}
+
+variable "serviceprinciple_key" {
+    default = "NRpBPeGNvf2C5tW5ZkO2~_CYEr4ar0xcqW"
+}
+
+variable "tenant_id" {
+    default = "a97a6880-5c33-4354-925a-f9f806acadea"
+}
+
+variable "subscription_id" {
+    default = "2d2449d2-a47e-4496-8377-be8b56f6b5c9"
+}
+
+
+variable "ssh_key" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7W84titHENBHs9KXf7MFfmqbQG+I9drlxBKV8w7Un001HSI03gEYr2yoA8Ubs2/Y7q+SH8k4/hZrygLJtBt6m2SI9/bTXNZbRxv0q4df2Thh3J400KLY/NueAJGMEItvqU4dJYgNA/tuofwkHK/YKin7IlhDstl8je8FabP1JH85dzdxnzCIqflA+NmTZZCVxpqqpNsCbPsb43eQIbd2Y4D0hcLIxvj+NYbIy3lh0p/xQDCtDXfhZvoGCYPcOeeYMB8xsPb+rYZkfJYIiKj7gDW5c2uoGXxh2ER325XTVQ7n+gWuzT7vCf4QGX3bOqcZH5ErJqxHjRNq0GWYS6z7X3dj4pRiTlq/QkcazXbYkfl85p72LImflrO6rJr61CJn8zCnk/nUdCec+71DHtVIBnPi7+jg2RoolomikgIxAUiX6QtKfAeP/dK4bTYwrN/oNAlmaEDjQKPn0kOvHqj4VsjWayJhu7bfuGO8WwDJRoRQFAg7HXwDW7qiSmaHlRp5FsuvlFw5hrG/KlQp0ulA/Fd3EukbqhXmTSCRQMXAUarbRIU5otDg2vKiIh2XlEGH78qEG73OSEHiRqYjyLHbXsKab4WDk7jYuwdWvxU8H+oJzj9yKg9JAdw6ON/fdKovdcZpyWYDbZ3AqR2NWhqogVvyfZuX9cD2JrpFQoUGP8w== risk\naikpr02@MUM8XQW6D3WXL"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "kubernetes_version" {
+    default = "1.19.11"
+}
